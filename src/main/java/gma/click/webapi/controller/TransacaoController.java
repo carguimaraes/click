d53e9d58318f38b6ae4baacaae78371666a8881e
carlos.GMA <carguimaraesgma@gmail.com>
2018-01-15 12:20:08 -0200

    Estrutura basica serviços

--- src/main/java/gma/click/webapi/controller/TransacaoController.java
+++ src/main/java/gma/click/webapi/controller/TransacaoController.java
@@ -1,5 +1,10 @@
 package gma.click.webapi.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
 public class TransacaoController {
 
 }
